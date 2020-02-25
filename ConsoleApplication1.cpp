83bcff746ff7f2f637b34fc91157528a734d584d
polinasevko <61377437+polinasevko@users.noreply.github.com>
2020-02-25 20:14:49 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,122 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#define M_PI		3.14159265358979323846
+
+// Exit the program
+void Exit(void)
+{
+	exit(0);
+}
+// Information about the version and author of the program
+void Inf(void)
+{
+	printf("Author: Sevko Polina Romanovna, gr. 953502.\nVersion: 1.0");
+	_getch();
+}
+// Counting the volume of the cone,
+void VolCone(int rad)
+{
+	double vol = 0;
+	vol = M_PI * pow(rad, 3) / 3;
+	printf("Volume of the cone is %f", vol);
+	_getch();
+}
+// Counting the length of the side of the square around which the circle is described.
+void SideSq(int rad) 
+{
+	double side = 0;
+	side = rad * pow(2, 0.5);
+	printf("Side of the square is %f", side);
+	_getch();
+}
+// Circumference calculation.
+void Circumference(int rad) 
+{
+	double circumference = 0;
+	circumference = 2 * M_PI * rad;
+	printf("Circumference is %f", circumference);
+	_getch();
+}
+// Calculation of the area of the sector of a circle formed by angle A
+void SecArea(int rad)
+{
+	double secArea = 0;
+	int A = 0;
+	printf("Enter the angle of the sector in degrees ");
+	scanf_s("%d", &A);
+	secArea = M_PI * pow(rad, 2) * A / 360;
+	printf("The area of the sector is %f", secArea);
+	_getch();
+}
+// Circle area calculation
+void CirArea(int rad) 
+{
+	double cirArea = 0;
+	cirArea = M_PI * pow(rad, 2);
+	printf("The area of the circle is %f", cirArea);
+	_getch();
+}
+// Radius input
+void RadInput(int *rad)
+{
+	printf("Enter the radius of the circle as an integer. ");
+	scanf_s("%d", &(*rad));
+}
+
+int main(void)
+{
+	int oper = 0;
+	int rad = 0;
+	RadInput(&rad);
+	while (1)
+	{
+		printf("Press the number whose number contains the operation you want to carry out.\n");
+		printf("1. Radius input\n");
+		printf("2. Circle area calculation.\n");
+		printf("3. Calculation of the area of the sector of a circle formed by angle A\n");
+		printf("4. Circumference calculation.\n");
+		printf("5. Counting the length of the side of the square around which the circle is described.\n");
+		printf("6. Counting the volume of the cone, the base of which is a circle, and the height is equal to the entered radius.\n");
+		printf("7. Information about the version and author of the program.\n");
+		printf("8. Exit the program.\n");
+		fflush(stdin);
+		scanf_s("%d", &oper);
+		if (oper == 1)
+		{
+			RadInput(&rad);
+		}
+		else if (oper == 2)
+		{
+			CirArea(rad);
+		}
+		else if (oper == 3) 
+		{
+			SecArea(rad);
+		}
+		else if (oper == 4)
+		{
+			Circumference(rad);
+		}
+		else if (oper == 5)
+		{
+			SideSq(rad);
+		}
+		else if (oper == 6)
+		{
+			VolCone(rad);
+		}
+		else if (oper == 7)
+		{
+			Inf();
+		}
+		else if (oper == 8)
+		{
+			Exit();
+		}
+		system("cls");
+	}
+}
---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,66 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+
+// Factorial
+int fact(int n) {
+	if (n == 1 || n == 0) {
+		return 1;
+	}
+	return n * fact(n - 1);
+}
+// Cycle
+void Cycle(double x, int n, double e) {
+	int counter = 1;
+	double sinx = 0;
+	int N = 0;
+	double series = 0;
+	sinx = sin(x);
+	while (counter <= n) {
+		series += (pow(-1, counter - 1) * pow(x, 2 * counter - 1)) / fact(2 * counter - 1);
+		if (fabs(series - sinx) < e && N == 0) {
+			N = counter;
+		}
+		counter++;
+	} 
+	printf("sin x = %lf\n\n", sinx);
+	printf("series = %lf\n", series);
+	if (N == 0) {
+		while (fabs(series - sinx) > e) {
+			series += (pow(-1, counter - 1) * pow(x, 2 * counter - 1)) / fact(2 * counter - 1);
+			counter++;
+		}
+		N = counter;
+	}
+	printf("when n = %d, the error is less than e = %lf\n\n", N, e);
+}
+// Recursion
+double Rec(double x, int n, double e) {
+	static double series = 0;
+	series += (pow(-1, n - 1) * pow(x, 2 * n - 1)) / fact(2 * n - 1);
+	n--;
+	if (n > 0) {
+		return Rec(x, n, e);
+	}
+	printf("series = %lf\n", series);
+	return 0;
+}
+
+int main()
+{
+	double x = 0;
+	int n = 0;
+	double e = 0;
+	printf("Enter x ");
+	scanf_s("%lf", &x);
+	printf("Enter the number of members in a row n ");
+	scanf_s("%d", &n);
+	printf("Enter math error e ");
+	scanf_s("%lf", &e);
+	Cycle(x, n, e);
+	Rec(x, n, e);
+	return 0;
+}
