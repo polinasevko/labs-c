dc1aa27f9f18c5d374fcc9a98860b068e966bb58
polinasevko <61377437+polinasevko@users.noreply.github.com>
2020-03-10 22:01:16 +0300

    Update laba2 2.1.20.cpp

--- laba2 2.1.20.cpp
+++ laba2 2.1.20.cpp
@@ -14,32 +14,28 @@ void Exit(void)
 // Information about the version and author of the program
 void Inf(void)
 {
-	printf("Author: Sevko Polina Romanovna, gr. 953502.\nVersion: 1.0");
-	_getch();
+	printf("Author: Sevko Polina Romanovna, gr. 953502.\nVersion: 1.0\n");
 }
 // Counting the volume of the cone,
 void VolCone(int rad)
 {
 	double vol = 0;
 	vol = M_PI * pow(rad, 3) / 3;
-	printf("Volume of the cone is %f", vol);
-	_getch();
+	printf("Volume of the cone is %f\n", vol);
 }
 // Counting the length of the side of the square around which the circle is described.
-void SideSq(int rad) 
+void SideSq(int rad)
 {
 	double side = 0;
 	side = rad * pow(2, 0.5);
-	printf("Side of the square is %f", side);
-	_getch();
+	printf("Side of the square is %f\n", side);
 }
 // Circumference calculation.
-void Circumference(int rad) 
+void Circumference(int rad)
 {
 	double circumference = 0;
 	circumference = 2 * M_PI * rad;
-	printf("Circumference is %f", circumference);
-	_getch();
+	printf("Circumference is %f\n", circumference);
 }
 // Calculation of the area of the sector of a circle formed by angle A
 void SecArea(int rad)
@@ -49,19 +45,17 @@ void SecArea(int rad)
 	printf("Enter the angle of the sector in degrees ");
 	scanf_s("%d", &A);
 	secArea = M_PI * pow(rad, 2) * A / 360;
-	printf("The area of the sector is %f", secArea);
-	_getch();
+	printf("The area of the sector is %f\n", secArea);
 }
 // Circle area calculation
-void CirArea(int rad) 
+void CirArea(int rad)
 {
 	double cirArea = 0;
 	cirArea = M_PI * pow(rad, 2);
-	printf("The area of the circle is %f", cirArea);
-	_getch();
+	printf("The area of the circle is %f\n", cirArea);
 }
 // Radius input
-void RadInput(int *rad)
+void RadInput(int* rad)
 {
 	printf("Enter the radius of the circle as an integer. ");
 	scanf_s("%d", &(*rad));
@@ -72,17 +66,17 @@ int main(void)
 	int oper = 0;
 	int rad = 0;
 	RadInput(&rad);
+	printf("Press the number whose number contains the operation you want to carry out.\n");
+	printf("1. Radius input\n");
+	printf("2. Circle area calculation.\n");
+	printf("3. Calculation of the area of the sector of a circle formed by angle A\n");
+	printf("4. Circumference calculation.\n");
+	printf("5. Counting the length of the side of the square around which the circle is described.\n");
+	printf("6. Counting the volume of the cone, the base of which is a circle, and the height is equal to the entered radius.\n");
+	printf("7. Information about the version and author of the program.\n");
+	printf("8. Exit the program.\n");
 	while (1)
 	{
-		printf("Press the number whose number contains the operation you want to carry out.\n");
-		printf("1. Radius input\n");
-		printf("2. Circle area calculation.\n");
-		printf("3. Calculation of the area of the sector of a circle formed by angle A\n");
-		printf("4. Circumference calculation.\n");
-		printf("5. Counting the length of the side of the square around which the circle is described.\n");
-		printf("6. Counting the volume of the cone, the base of which is a circle, and the height is equal to the entered radius.\n");
-		printf("7. Information about the version and author of the program.\n");
-		printf("8. Exit the program.\n");
 		fflush(stdin);
 		scanf_s("%d", &oper);
 		if (oper == 1)
@@ -93,7 +87,7 @@ int main(void)
 		{
 			CirArea(rad);
 		}
-		else if (oper == 3) 
+		else if (oper == 3)
 		{
 			SecArea(rad);
 		}
@@ -117,6 +111,5 @@ int main(void)
 		{
 			Exit();
 		}
-		system("cls");
 	}
 }
